63d5919dacc8a7bd2eb5f8231cb86d1cecdd55a3
Ангелина Кузнецова <angelkuzk2004k@gmail.com>
2025-09-21 16:22:23 +0300

    Картинка

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
-from flask import Flask
+from flask import Flask, url_for
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/lab1")
+def lab1():
+    return """<!doctype html>
+        <html>
+            <body>
+                <ul>
+                    <h1>Лабораторная работа 1</h1>
+                    <li><a href="/author">author</a></li>
+                    <li><a href="/web">web</a></li>
+                    <li><a href="/image">image</a></li>
+                </ul>
+            </body>
+        </html>"""    
+
 @app.route("/web")
 def web():
     return """<!doctype html>
@@ -9,6 +23,8 @@ def web():
             <body>
                 <h1>web-сервер на flask</h1>
                 <a href="/author">author</a>
+                <br>
+                <a href="/lab1">Назад к главной</a>
             </body>
         </html>"""
 
@@ -17,7 +33,7 @@ def author():
     name = "Кузнецова Ангелина Андреевна"
     group = "ФБИ-33"
     faculty = "ФБ"
-    
+
     return """<!doctype html>
         <html>
             <body>
@@ -25,5 +41,23 @@ def author():
                 <p>Группа: """ + group + """</p>
                 <p>Факультет: """ + faculty + """</p>
                 <a href="/web">web</a>
+                <br>
+                <a href="/lab1">Назад к главной</a>
             </body>
         </html>"""
+    
+
+@app.route('/image') 
+def image():
+    path = url_for("static", filename="oak.jpg")
+    return '''
+<!doctype html>
+<html>
+    <body>
+        <h1>Дуб</h1>
+        <img src="''' + path + '''" width="800">
+        <br>
+        <a href="/lab1">Назад к главной</a>
+    </body>
+</html>
+'''
