20d3b1237bc406fd1f7143f554f96e151d08672b
Ангелина Кузнецова <angelkuzk2004k@gmail.com>
2025-09-21 16:59:22 +0300

    перенаправление

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, request
+from flask import Flask, url_for, request, redirect
 import datetime
 app = Flask(__name__)
 
@@ -85,3 +85,6 @@ def counter():
     </body>
 </html>
 '''
+@app.route("/info")
+def info():    
+    return redirect("/author")
