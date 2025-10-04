8d6adec0e0ae22b2a7e5673a32881511aaf9b606
Ангелина Кузнецова <angelkuzk2004k@gmail.com>
2025-10-04 18:43:54 +0300

    Имя студента

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, request, redirect, abort
+from flask import Flask, url_for, request, redirect, abort, render_template
 import datetime
 app = Flask(__name__)
 
@@ -570,3 +570,7 @@ def add_flower(name):
     </body>
 </html>
 '''
+@app.route('/lab2/example')
+def example():
+    name = 'Ангелина Кузнецова'
+    return render_template('example.html', name=name)
